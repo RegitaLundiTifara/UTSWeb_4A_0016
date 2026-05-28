44bd33c1ac16bb057a4744bc233103393ba64b47
RegitaLundiTifara <regita.tifara24@student.uisi.ac.id>
2026-05-29 01:09:16 +0700

    menambahkan section order

---
+++ script.js
@@ -0,0 +1,15 @@
+// Notifikasi Order
+$("#orderBtn").click(function(){
+
+    alert("Thank you for ordering!");
+
+});
+
+// Pengisian Contact Form
+$("#contactForm").submit(function(e){
+
+    e.preventDefault();
+
+    alert("Message sent successfully!");
+
+});
